3c9c797aaf64a886b4823a9d12eebd5efa7599e8
mauricio viana <mauriycf@gmail.com>
2017-09-13 19:12:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
   }if (string == string.toUpperCase()) {
     return "YES INDEED!";
-  }if (string == "I Love you, Grandma.") {
+  }if (string == "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
