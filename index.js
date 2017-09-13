83be9dce689069d9fb5fd8af4092dfdc689f4ef2
mauricio viana <mauriycf@gmail.com>
2017-09-13 19:10:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string){
 function logWhisper(string){
   return console.log('hello');
 }
+
+function sayHiToGrandma(string){
+  if (string == string.toLowerCase()) {
+    return "I can't hear you!";
+  }if (string == string.toUpperCase()) {
+    return "YES INDEED!";
+  }if (string == "I Love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
